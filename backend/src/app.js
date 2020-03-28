b7f3559c28336faf9cb842e66c209184b56e6569
Lucas Leonel <lucasap_98@hotmail.com>
2020-03-27 22:31:48 -0300

    Testes e Validações

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {errors} = require('celebrate');
 const cors = require('cors');
 const routes = require('./routes');
 
@@ -7,5 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use (routes);
 
+app.use(errors());
 
-app.listen(3333);
+
+module.exports = app;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const routes = express.Router();
 const crypto = require('crypto');
 const connection = require('./database/connection');
@@ -9,17 +10,37 @@ const SessionController = require('./controllers/SessionController');
 
 
 
-
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email : Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  }),
+}), OngController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
 routes.post('/session', SessionController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.get('/profile', ProfileController.index);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+  })
+}), IncidentController.index);
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+          authorization: Joi.string().required(),
+      }).unknown(),
+}),ProfileController.index);
 
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+     id: Joi.number().required(), 
+  }),
+}), IncidentController.delete)
 
 module.exports = routes;
